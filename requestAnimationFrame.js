13eaf751bf4745e9ca22b528fb4a64f0a7210c87
timurridjanovic <timur.ridjanovic@gmail.com>
2019-05-10 04:07:52 -0400

    add editorconfig

--- requestAnimationFrame.js
+++ requestAnimationFrame.js
@@ -1,10 +1,11 @@
 let lastTime = 0
 const requestAnimationFrame = callback => {
-	const currTime = new Date().getTime()
-	const timeToCall = Math.max(0, 16 - (currTime - lastTime))
-	const id = setTimeout(() => callback(currTime + timeToCall), timeToCall)
-	lastTime = currTime + timeToCall
-	return id
+  const currTime = new Date().getTime()
+  const timeToCall = Math.max(0, 16 - (currTime - lastTime))
+  const id = setTimeout(() => callback(currTime + timeToCall), timeToCall)
+  lastTime = currTime + timeToCall
+  return id
 }
 
 module.exports = requestAnimationFrame
+
